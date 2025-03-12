7dfe7d529acf6c06aaabd4954a99bd95c2f71e2c
wmmeg <wmmegal@gmail.com>
2025-03-12 20:13:05 +0200

    main

--- src/js/common.js
+++ src/js/common.js
@@ -1,3 +1,32 @@
-import 'virtual:svg-icons-register'
-import '../css/tailwind.css'
+import 'virtual:svg-icons-register';
+import '../css/tailwind.css';
+import { Fancybox } from '@fancyapps/ui';
+import '@fancyapps/ui/dist/fancybox/fancybox.css';
 
+Fancybox.bind('[data-fancybox]');
+
+document.addEventListener('click', (e) => {
+    const btn = e.target.closest('.btn-menu');
+
+    if (btn == null) {
+        return;
+    }
+
+    btn.classList.toggle('active');
+
+    const menu = btn.nextElementSibling;
+
+    menu.classList.toggle('open');
+});
+
+document.addEventListener('change', (e) => {
+    if (!e.target.closest('[name=rule]')) {
+        return;
+    }
+
+    const checkbox = e.target.closest('[name=rule]'),
+        form = checkbox.closest('form'),
+        btn = form.querySelector('[type=submit]');
+
+    btn.disabled = !checkbox.checked;
+});
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,30 +1,34 @@
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-        './src/**/*.{html,js}',
-    ],
+    content: ['./src/**/*.{html,js}'],
     theme: {
         container: {
             center: true,
-            padding: '15px',
+            padding: '16px',
             screens: {
                 sm: '375px',
                 md: '575px',
                 lg: '767px',
-                xl: '1200px',
+                xl: '1500px',
             },
         },
         colors: {
-            'primary': '#1752ca',
-            'black': '#16140f',
-            'white': '#FFFFFF',
+            primary: '#797979',
+            black: '#000',
+            white: '#FFFFFF',
         },
         extend: {
             fontSize: {
-                '4xl': ['40px', '1'],
+                lg: ['18px', '1.1'],
+                xl: ['28px', '1.1'],
+                '2xl': ['32px', '1.1'],
+                '3xl': ['45px', '1.1'],
+                '4xl': ['57px', '1.1'],
             },
         },
     },
-    plugins: [],
+    plugins: [forms, typography],
 };
-
